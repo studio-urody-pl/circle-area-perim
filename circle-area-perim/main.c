f9be6d4e1aba9d768007f0f75c9bcf48f2c913b0
studio-urody.pl <101935710+studio-urody-pl@users.noreply.github.com>
2022-12-15 11:30:24 +0100

    Initial Commit

---
+++ circle-area-perim/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  circle-area-perim
+//
+//  Created by Tomasz Uroda on 15/12/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
